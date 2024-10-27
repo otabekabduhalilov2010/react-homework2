da168921e18f46ca4498355d0e1c12f2eb57da3c
otabekabduhalilov2010 <otabekabduhalilov80@gmail.com>
2024-10-27 17:15:20 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <div className="wrapper">
+            <div className="card">
+              <img
+                src="https://www.sneakerhype.eu/cdn/shop/products/file_2d5615e9-91ed-4802-8764-cecb01d0edc4.png?v=1710155044" alt=""/>
+              <div className="title">
+                <h3>Nike x off-white</h3>
+                <p>the 10 jorden 1 off white chicago</p>
+                <p>999$</p>
+              </div>
+              <div className="clicker">
+                <button>Buy now</button>
+                
+                
+              </div>
+            </div>
+
+            <div className="card2">
+              <img src="https://sneakerask.nl/cdn/shop/products/air-jordan-1-mid-blue-mint-1-1000.png?v=1652360820" alt="" />
+              <div className="title">
+                <h3>Nike x off-white</h3>
+                <p>the 10 jorden 1 off white chicago</p>
+                <p>999$</p>
+              </div>
+              <div className="clicker">
+                <button>Buy now</button>
+          
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
